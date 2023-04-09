ca270e6488a9ecba029f30a42265543003f3bf76
LathaThavamai <latha.thavamani@gmail.com>
2023-04-09 03:27:15 +0200

    Movie Detail & Movie Video Source - API implemented

--- routes/movies/movies.js
+++ routes/movies/movies.js
@@ -5,7 +5,7 @@ const moviesRouter = Router();
 
 moviesRouter.get('/trending', async (req, res) => {
     let { data } = await axios.get(
-        `https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.TMDB_API_KEY}`
+        `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_API_KEY}`
     );
     res.json(data.results)
 
@@ -16,6 +16,26 @@ moviesRouter.get('/toprated', async (req, res) => {
         `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.TMDB_API_KEY}`
     );
     res.json(data.results)
+
+})
+
+moviesRouter.get('/detail', async (req, res) => {
+    let movieId = req.query.id;
+    let { data } = await axios.get(
+        //`https://api.themoviedb.org/3/tv/${movieId}?api_key=${process.env.TMDB_API_KEY}`
+        `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.TMDB_API_KEY}`
+    );
+    res.json(data)
+
+})
+
+moviesRouter.get('/video', async (req, res) => {
+    let movieId = req.query.id;
+    let { data } = await axios.get(
+        //`https://api.themoviedb.org/3/tv/${movieId}?api_key=${process.env.TMDB_API_KEY}`
+        `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${process.env.TMDB_API_KEY}`
+    );
+    res.json(data.results[0])
 
 })
 
