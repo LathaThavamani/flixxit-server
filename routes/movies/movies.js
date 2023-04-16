e312c04773480ae19c5bb20e3ccb466d807d1b7d
LathaThavamai <latha.thavamani@gmail.com>
2023-04-16 19:21:26 +0200

    TMDB env variable created

--- routes/movies/movies.js
+++ routes/movies/movies.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import axios from "axios";
 
+//const TMDB_URL = process.env.TMDB_API_URL
 const moviesRouter = Router();
 
 moviesRouter.get('/trending', async (req, res) => {
     let { data } = await axios.get(
-        `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_API_KEY}`
+        `${process.env.TMDB_API_URL}/movie/popular?api_key=${process.env.TMDB_API_KEY}`
     );
     res.json(data.results)
 
@@ -13,7 +14,7 @@ moviesRouter.get('/trending', async (req, res) => {
 
 moviesRouter.get('/toprated', async (req, res) => {
     let { data } = await axios.get(
-        `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.TMDB_API_KEY}`
+        `${process.env.TMDB_API_URL}/movie/top_rated?api_key=${process.env.TMDB_API_KEY}`
     );
     res.json(data.results)
 
@@ -22,7 +23,7 @@ moviesRouter.get('/toprated', async (req, res) => {
 moviesRouter.get('/detail', async (req, res) => {
     let movieId = req.query.id;
     let { data } = await axios.get(
-        `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.TMDB_API_KEY}`
+        `${process.env.TMDB_API_URL}/movie/${movieId}?api_key=${process.env.TMDB_API_KEY}`
     );
     res.json(data)
 
@@ -31,7 +32,7 @@ moviesRouter.get('/detail', async (req, res) => {
 moviesRouter.get('/video', async (req, res) => {
     let movieId = req.query.id;
     let { data } = await axios.get(
-        `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${process.env.TMDB_API_KEY}`
+        `${process.env.TMDB_API_URL}/movie/${movieId}/videos?api_key=${process.env.TMDB_API_KEY}`
     );
     res.json(data.results[0])
 
@@ -40,7 +41,7 @@ moviesRouter.get('/video', async (req, res) => {
 moviesRouter.get('/search', async (req, res) => {
     let searchText = req.query.searchText;
     let { data } = await axios.get(
-        `https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_API_KEY}&query=${searchText}`
+        `${process.env.TMDB_API_URL}/search/movie?api_key=${process.env.TMDB_API_KEY}&query=${searchText}`
     );
     res.json(data.results)
 })
